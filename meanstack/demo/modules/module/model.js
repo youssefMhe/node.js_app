73b9748ef3e10facf36da93f4fcb281dbb33e62d
unknown <youssef.mhenni1@gmail.com>
2023-03-15 12:45:16 +0100

    modules

---
+++ meanstack/demo/modules/module/model.js
@@ -0,0 +1,16 @@
+//require , export , modules , __dirname , __filename
+
+console.log(__dirname)
+console.log(__filename)
+
+
+let names = ['jill', 'slim ', 'mike']
+let score = [100, 55, 7887]
+
+
+exports.names = names
+
+module.exports.score = score
+
+
+console.log(module)
